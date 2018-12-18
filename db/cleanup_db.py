0ff8a4eb6ffdec02ab4bf028542b2c18cffc7676
iadgovuser8 <iadgovuser8@users.noreply.github.com>
2018-12-18 15:20:23 -0500

    Reorganzing files and renaming. Added script to handle when DB is not closed properly

---
+++ db/cleanup_db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+try:
+    DBCONN = sqlite3.connect("..\\WSUS_Update_Data.db",
+                            check_same_thread=False, isolation_level=None)
+
+    DBCONN.execute("pragma journal_mode=wal")
+    DBCONN.execute("pragma synchronous=NORMAL")
+
+    DBCONN.commit()
+    
+    DBCONN.close()
+except sqlite3.Error as error:
+    print("Error caught: ", error.args[0])
+except:
+    print("Error caught")
+
